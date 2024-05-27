cf2db96f54a16decfce33ed9db8ca48276e60961
Raddames <raddamestonui48@gmail.com>
2024-05-27 16:08:06 +0300

    done

--- lib/person.py
+++ lib/person.py
@@ -16,19 +16,20 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    def __init__(self, name = "Radde", job = "sales") -> None:
+    def __init__(self, name="Raddames", job='Marketing'):
         self.name = name
-        self.job =job
+        self.job = job
 
     def get_name(self):
-       return self._name
-
+        return self._name
+    
     def set_name(self, name):
         if isinstance(name, str) and 1 <= len(name) <= 25:
             self._name = name.title()
         else:
             print("Name must be string between 1 and 25 characters.")
-    name =  property(get_name, set_name)
+
+    name = property(get_name, set_name)
 
     def get_job(self):
         return self._job
@@ -38,5 +39,5 @@ class Person:
             self._job = job
         else:
             print("Job must be in list of approved jobs.")
-    
+
     job = property(get_job, set_job)
